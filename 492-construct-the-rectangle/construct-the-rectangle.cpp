f7c176eec41b2d0996392e1ee63ddedfa3fbbd84
Aditya Sarpotdar <92214487+aditya-sar@users.noreply.github.com>
2023-09-29 18:04:02 +0530

    Time: 101 ms (12.47%) | Memory: 6.5 MB (12.47%) - LeetSync

--- 492-construct-the-rectangle/construct-the-rectangle.cpp
+++ 492-construct-the-rectangle/construct-the-rectangle.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<int> constructRectangle(int area) {
         vector<int> ans;
-        int maxDiff = INT_MAX, dividend, divisor;
+        int maxDiff = INT_MAX, larger, smaller;
         for(int i=1; i<=area/2+1; i++) {
             if(area%i == 0) {
                 if(abs(i - area/i) < maxDiff) {
                     maxDiff = abs(i - area/i);
-                    dividend = max(i, area/i);
-                    divisor = min(i, area/i);
+                    larger = max(i, area/i);
+                    smaller = min(i, area/i);
                 }
             }
         }
-        return {dividend, divisor};
+        return {larger, smaller};
     }
 };
