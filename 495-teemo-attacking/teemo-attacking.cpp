bdad3257b0125da1ad4784cd4c42a2142751a0fd
Aditya Sarpotdar <92214487+aditya-sar@users.noreply.github.com>
2023-09-29 18:13:30 +0530

    Time: 38 ms (14.52%) | Memory: 26.3 MB (9.57%) - LeetSync

---
+++ 495-teemo-attacking/teemo-attacking.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int n, ans = 0;
+
+    int findPoisonedDuration(vector<int>& time, int d) {
+        n = time.size();
+        int end = -1;
+        for(int i=0; i<n; i++) {
+            ans += d;
+            if(end >= time[i]) {
+                ans -= (abs(end-time[i]) + 1);
+            }
+            end = time[i]+d-1;
+        }
+        return ans;
+    }
+};
