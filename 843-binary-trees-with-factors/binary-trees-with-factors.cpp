35852e409a9a1baa41f975fb69a5eed0e4e14e17
Aditya Sarpotdar <92214487+aditya-sar@users.noreply.github.com>
2023-10-26 21:06:05 +0530

    Time: 19 ms (95.28%) | Memory: 10.2 MB (35.04%) - LeetSync

---
+++ 843-binary-trees-with-factors/binary-trees-with-factors.cpp
@@ -0,0 +1,36 @@
+
+class Solution {
+public:
+	const int MOD = 1e9 + 7;
+	int n;
+
+    int numFactoredBinaryTrees(vector<int>& a) {
+    	n = a.size();
+        sort(a.begin(), a.end());
+        unordered_set<int> s(a.begin(), a.end());
+        unordered_map<int, int> mp;
+        for(int x : a) {
+        	mp[x] = 1;
+        }
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if (a[j] > sqrt(a[i])) {
+                	break;
+                }
+                if(a[i] % a[j] == 0 && s.find(a[i] / a[j]) != s.end()) {
+                    long long temp = (long long)mp[a[j]] * mp[a[i] / a[j]];
+                    if (a[i] / a[j] == a[j]) {
+                        mp[a[i]] = (mp[a[i]] + temp) % MOD;
+                    } else {
+                        mp[a[i]] = (mp[a[i]] + temp * 2) % MOD;
+                    }
+                }
+            }
+        }
+        int ans = 0;
+        for (auto pr : mp) {
+            ans = (ans + pr.second) % MOD;
+        }
+        return ans;
+    }
+};
