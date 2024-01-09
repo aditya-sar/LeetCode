57f7800c670d6ec730fb53f688ac1ba679344102
Aditya Sarpotdar <92214487+aditya-sar@users.noreply.github.com>
2024-01-09 15:35:09 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (95.92%) - LeetSync

---
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> v1, v2;
+
+    void solve(TreeNode *root, vector<int> &v) {
+        if(!root) {
+            return;
+        }
+        if(!root -> left && !root -> right) {
+            v.push_back(root -> val);
+        }
+        solve(root -> left, v);
+        solve(root -> right, v);
+    }
+
+
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        solve(root1, v1);
+        solve(root2, v2);
+        if(v1.size() != v2.size()) {
+            return false;
+        }
+        for(int i=0; i<v1.size(); i++) {
+            if(v1[i] != v2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
