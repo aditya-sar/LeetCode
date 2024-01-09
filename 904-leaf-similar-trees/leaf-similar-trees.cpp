522358d5155b9b9a9e24eda0a0be4a2380c93ae2
Aditya Sarpotdar <92214487+aditya-sar@users.noreply.github.com>
2024-01-09 15:35:51 +0530

    Time: 7 ms (20.29%) | Memory: 13.5 MB (19.23%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.cpp
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -17,14 +17,6 @@ public:
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
         solve(root1, v1);
         solve(root2, v2);
-        if(v1.size() != v2.size()) {
-            return false;
-        }
-        for(int i=0; i<v1.size(); i++) {
-            if(v1[i] != v2[i]) {
-                return false;
-            }
-        }
-        return true;
+        return v1 == v2;
     }
 };
