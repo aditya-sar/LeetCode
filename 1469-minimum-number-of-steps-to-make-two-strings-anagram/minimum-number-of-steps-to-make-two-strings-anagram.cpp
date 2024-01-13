fd9e82b8f30fa18621baa9093cabaa26ff5ca16a
Aditya Sarpotdar <92214487+aditya-sar@users.noreply.github.com>
2024-01-13 15:36:33 +0530

    Time: 71 ms (47.37%) | Memory: 17.2 MB (19.43%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -6,11 +6,9 @@ public:
 
     int minSteps(string s, string t) {
         n = s.size();
-        for(char &c : s) {
-            mp1[c]++;
-        }
-        for(char &c : t) {
-            mp2[c]++;
+        for(int i=0; i<n; i++) {
+            mp1[s[i]]++;
+            mp2[t[i]]++;
         }
         for(char c='a'; c<='z'; c++) {
             ans += abs(mp1[c] - mp2[c]);
