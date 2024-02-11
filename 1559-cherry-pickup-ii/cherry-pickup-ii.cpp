4ca61b22df6fbca0dc603d45351ea0d490518212
Aditya Sarpotdar <92214487+aditya-sar@users.noreply.github.com>
2024-02-11 13:05:50 +0530

    Time: 30 ms (97.34%) | Memory: 12.5 MB (77.44%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int px[3] = {0,-1,1};
     int dp[71][71][71];
+    int n, m;
 
-    int dfs(vector<vector<int>>& a, int pos, int c1, int c2, int n, int m) {
+    int dfs(vector<vector<int>>& a, int pos, int c1, int c2) {
         if(pos == n) {
             return 0;
         }
@@ -16,17 +17,17 @@ public:
         int ans = 0;
         for(int k=0; k<3; k++){
             for(int r=0; r<3; r++){
-                ans = max(ans, dfs(a, pos+1, c1 + px[k], c2 + px[r] , n, m));
+                ans = max(ans, dfs(a, pos+1, c1 + px[k], c2 + px[r]));
             }
         }
-        ans += (c1==c2) ? a[pos][c1] : a[pos][c1] + a[pos][c2];
+        ans += (c1 == c2) ? a[pos][c1] : a[pos][c1] + a[pos][c2];
         return dp[pos][c1][c2] = ans;
     }
 
     int cherryPickup(vector<vector<int>>& a) {
-        int n = a.size(); 
-        int m  = a[0].size(); 
+        n = a.size(); 
+        m  = a[0].size(); 
         memset(dp,-1,sizeof dp);
-        return dfs(a, 0, 0, m-1, n, m);
+        return dfs(a, 0, 0, m-1);
     }
 };
