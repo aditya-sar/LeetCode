46141d0bddf5a936ea0aebb5a071b1267f83bbb6
Aditya Sarpotdar <92214487+aditya-sar@users.noreply.github.com>
2024-03-13 10:39:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (41.86%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int left = 0, right = n * (n+1) / 2;
+        for(int i=1; i<=n; i++) {
+            left = i*(i+1)/2;
+            right = n*(n+1)/2 - i*(i-1)/2;
+            if(left == right) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
